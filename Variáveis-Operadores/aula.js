026cbe1aa2bcd284c18ecf8e46a95c24c391438c
Nome <miranhaomi@gmail.com>
2023-11-10 16:54:21 -0300

    Variáveis-Operadores
    Commit relacionado a tudo feito durante as aulas de Variáveis e Operadores do bootcamp de desenvolvimento de jogos da Dio

---
+++ Variáveis-Operadores/aula.js
@@ -0,0 +1,19 @@
+
+let variavel = 10; // let muda (variável)
+const pi = 3.14; // const não muda (constante)
+let produto = variavel * pi;
+let soma = variavel + pi;
+let subtrai = variavel - pi;
+let dividir = variavel/pi;
+
+/* pi = 3.15 */
+console.log(produto);
+console.log(soma);
+console.log(subtrai);
+console.log(dividir)
+
+
+
+
+
+console.log("imprimir");
---
+++ Variáveis-Operadores/desafio.js
@@ -0,0 +1,8 @@
+const precoCombus = 5;
+const kmporlitro = 10;
+const distancia = 50;
+
+const litrosconsumidos = 50 / 10;
+const valorgasto = precoCombus * litrosconsumidos;
+console.log(valorgasto);
+
